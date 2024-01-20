71352a9bc89d5a89ef088280e7e2330547ad723d
Arthur Moyo <moyoarthur22@gmail.com>
2024-01-20 14:53:08 +0200

    React tutorial

---
+++ src/app/square.js
@@ -0,0 +1,13 @@
+'use client';
+import { useState } from 'react';
+export function Square({value, handleClick}) {
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
